7c71770326839d78a3e5d6dd6c3c72b26f453b88
Anders Einar Hilden <hildenae@gmail.com>
2017-12-24 16:02:29 +0100

    Move folder and add .noborg so we can skip backing up certs

--- config.sh
+++ config.sh
@@ -2,6 +2,6 @@ CT_PATH="$HOME/ct"
 STORAGE_PATH="$PWD/data"
 THREADS=8
 
-MCN_TOOLS="/home/hildenae/Documents/Kildekode/mcn-tools"
+MCN_TOOLS="/home/hildenae/Documents/Kildekode/mcn/mcn-tools"
 
 DOMAINS="mcn-source-ct.list"
