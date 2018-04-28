93fc0b52f60c532de94d2ef1ea43a44c53b2a158
Anders Einar Hilden <hildenae@gmail.com>
2018-04-28 20:35:09 +0000

    add print-func to from_to and function for common code

--- from_to.sh
+++ from_to.sh
@@ -2,11 +2,16 @@
 SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # This way we can work with realtive paths in the rest of the script
 cd "${SOURCE_DIR}"
-
 source config.sh
 
-if [ "x$1" != "x" ] && [ "x$2" != "x" ] && [ "$1" -le "$2" ]; then
-   cat logs.txt | grep -P " \d{${1},${2}}$" | while read LOG; 
+function select_logs {
+  cat logs.txt | grep -P " \d{${1},${2}}$"
+}
+
+if [ "x$1" != "x" ] && [ "x$2" != "x" ] && [ "$1" -le "$2" ] && [ "x$3" != "x"  ]; then 
+   select_logs "$1" "$2" | sort -k 2 -n 
+elif [ "x$1" != "x" ] && [ "x$2" != "x" ] && [ "$1" -le "$2" ]; then
+   select_logs "$1" "$2" | while read LOG; 
 	do ./get_certs.sh "$LOG"; 
    done
 else
