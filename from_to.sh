81b657a2aad6adeb69b9253f53f18d2e3126a6de
Anders Einar Hilden <hildenae@gmail.com>
2018-04-28 20:01:29 +0000

    Get data from config, and run get_certs directly, so we dont have to write oneliner

--- from_to.sh
+++ from_to.sh
@@ -1,6 +1,14 @@
 #! /bin/bash
+SOURCE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# This way we can work with realtive paths in the rest of the script
+cd "${SOURCE_DIR}"
+
+source config.sh
+
 if [ "x$1" != "x" ] && [ "x$2" != "x" ] && [ "$1" -le "$2" ]; then
-   cat logs.txt | grep -P " \d{${1},${2}}$"
+   cat logs.txt | grep -P " \d{${1},${2}}$" | while read LOG; 
+	do ./get_certs.sh "$LOG"; 
+   done
 else
     echo 'Missing $1 or $2, or $2 was smaller than $1' 1>&2
     exit 1
